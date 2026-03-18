d9f42381e6b6063f3e130b9fa3b679e30a55daa2
Maxim Mudryi <m.mudryi@my.com>
2026-03-18 19:16:11 +0200

    Enhance install script for OpenVPN setup
    Updated installation script to include additional packages and modify OpenVPN configuration settings.

--- install_zaborona.sh
+++ install_zaborona.sh
@@ -1,46 +1,51 @@
 #!/bin/sh
 # 1. Установка пакетов
 apk update
-apk add openvpn-openssl luci-app-openvpn ca-bundle ca-certificates
+apk add openvpn-openssl
+apk add luci-app-openvpn
+apk add luci-i18n-openvpn-ru
+apk add libustream-openss #Нужен для нормального функционирования wget
 
-# 2. Сертификаты
-mkdir -p /etc/openvpn
-wget --no-check-certificate -q "https://zaborona.help/ca.crt" -O /etc/openvpn/ca.crt
-wget --no-check-certificate -q "https://zaborona.help/zaborona-help.crt" -O /etc/openvpn/zaborona-help.crt
-wget --no-check-certificate -q "https://zaborona.help/zaborona-help.key" -O /etc/openvpn/zaborona-help.key
+wget --no-check-certificate "https://zaborona.help/ca.crt" -O /etc/openvpn/ca.crt
+wget --no-check-certificate "https://zaborona.help/zaborona-help.crt" -O /etc/openvpn/zaborona-help.crt
+wget --no-check-certificate "https://zaborona.help/zaborona-help.key" -O /etc/openvpn/zaborona-help.key
 
-# 3. Чистый конфиг сети (создаем само устройство)
 uci set network.zaborona_help=interface
 uci set network.zaborona_help.proto='none'
-uci set network.zaborona_help.device='tun0'
+uci set network.zaborona_help.ifname='tun0'
+uci set network.zaborona_help.auto='1'
+uci commit network
 
-# 4. Конфиг VPN (Европа/Big Routes)
+# Удаляю Default конфиги, чтобы не "мозолили" глаза. 
+uci delete openvpn.custom_config
+uci delete openvpn.sample_server
+uci delete openvpn.sample_client
+
 uci set openvpn.zaborona_help=openvpn
 uci set openvpn.zaborona_help.client='1'
-uci set openvpn.zaborona_help.enabled='1'
-uci set openvpn.zaborona_help.dev='tun0'
-uci set openvpn.zaborona_help.proto='udp'
-uci set openvpn.zaborona_help.remote='srv0bigroutes.vpn.zaboronahelp.pp.ua 1194'
-uci set openvpn.zaborona_help.resolv_retry='infinite'
+uci set openvpn.zaborona_help.reneg_sec='0'
+uci set openvpn.zaborona_help.verb='0' #убрал логирование по умолчанию — 3
 uci set openvpn.zaborona_help.nobind='1'
-uci set openvpn.zaborona_help.persist_key='1'
-uci set openvpn.zaborona_help.persist_tun='1'
 uci set openvpn.zaborona_help.remote_cert_tls='server'
-uci set openvpn.zaborona_help.auth='sha1'
+uci set openvpn.zaborona_help.dev='tun0'
+uci set openvpn.zaborona_help.remote='srv0bigroutes.vpn.zaboronahelp.pp.ua'
+uci set openvpn.zaborona_help.proto='tcp-client'
 uci set openvpn.zaborona_help.cipher='AES-128-CBC'
-uci set openvpn.zaborona_help.data_ciphers='AES-128-GCM:AES-128-CBC'
 uci set openvpn.zaborona_help.ca='/etc/openvpn/ca.crt'
 uci set openvpn.zaborona_help.cert='/etc/openvpn/zaborona-help.crt'
 uci set openvpn.zaborona_help.key='/etc/openvpn/zaborona-help.key'
-uci set openvpn.zaborona_help.verb='3'
-uci set openvpn.zaborona_help.mssfix='1300'
-uci commit
+uci set openvpn.zaborona_help.enabled='1'
+uci set openvpn.zaborona_help.auth_nocache='1'
+uci set openvpn.zaborona_help.pull_filter='ignore ifconfig-ipv6' #отключил поддержку IPv6
+uci add_list openvpn.zaborona_help.pull_filter='ignore route-ipv6' #отключил поддержку IPv6
 
-# 5. Привязка к файрволу (чтобы сайты открывались)
-WAN_ZONE=$(uci show firewall | grep ".name='wan'" | cut -d'[' -f2 | cut -d']' -f1)
-[ -z "$WAN_ZONE" ] && WAN_ZONE=1
-uci add_list firewall.@zone[$WAN_ZONE].network='zaborona_help'
+uci commit openvpn
+# Обратить внимание если есть pppoe и wan6 интерфейс, то будет 'wan6 pppoe wan zaborona_help'.
+uci set firewall.@zone[1].network='wan zaborona_help' 
 uci commit firewall
 
-/etc/init.d/network restart
-/etc/init.d/openvpn restart
+/etc/init.d/openvpn enable
+/etc/init.d/openvpn reload
+/etc/init.d/firewall reload
+/etc/init.d/network reload
+
